c32c1522eaca3d792b927c32cec82bb3aba4f9d7
Yeji-Yoon <angelayoon0102@gmail.com>
2025-02-05 16:44:23 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector('#text')
+    text.textContent="This is really cool!"
+    
+  });
+  
+  
